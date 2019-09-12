8bc353014a946060cc80cbcdba67fdb639ce08a5
VincentXu521 <xu-2@qq.com>
2019-09-13 00:16:30 +0800

    done P42

--- P42_TrapRainWater.cpp
+++ P42_TrapRainWater.cpp
@@ -7,19 +7,45 @@
 
 #include<vector>
 #include<map>
+#define MIN(a,b) (a>b?b:a)
+
 using namespace std;
 
-
+// Solution 1: count by rol one by one;
 class Solution {
 public:
 	int trap(vector<int>& height) {
-		map<int, int> max_value;
-		int n = height.size();
-		if ( n< 3)return 0;
-		if (height[0] > height[1])max_value[0] = 1;
-		if (height[n - 1] > height[n - 2])max_value[n - 1] = 1;
-		for (int i = 1; i < n-1; i++) {
-			if (height[i] >= height[i - 1] && height[i] >= height[i + 1])max_value[i] = 1;
+
+		int sum = 0;
+		// int(height.size() - 1) cannot be replaced by height.size() - 1
+		// 注意数据类型，无符号的0减去1不能变成负数，变为 max unsigned int；
+		for (int i = 1; i < int(height.size() - 1); i++) {
+
+			int curr = height.at(i);
+			int max_left = curr;
+			int max_right = curr;
+
+			// the max_value at left of curr
+			for (int j = i - 1; j >= 0; j--) {
+				if (height.at(j) > max_left) {
+					max_left = height.at(j);
+				}
+			}
+			// the max_value at right of curr
+			for (int j = i + 1; j < height.size(); j++) {
+				if (height.at(j) > max_right) {
+					max_right = height.at(j);
+				}
+			}
+			// min value of max_left and max_right
+			int min = MIN(max_left, max_right);
+
+			// add the water at current col
+			if (min > curr) {
+				sum += (min - curr);
+			}
+
 		}
+		return sum;
 	}
 };
